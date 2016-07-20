133182d6c13baf033f4b8de5750a341d529817f1
Yeung Yiu Hung <yeungyiuhung@cherrypicks.com>
2016-07-20 12:46:42 +0800

    config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 
 // The server port - the port to run Pokemon Showdown under
 exports.port = 80;
-
+exports.bindaddress = 'cp-pokemon-fight.herokuapp.com'
 // proxyip - proxy IPs with trusted X-Forwarded-For headers
 //   This can be either false (meaning not to trust any proxies) or an array
 //   of strings. Each string should be either an IP address or a subnet given
