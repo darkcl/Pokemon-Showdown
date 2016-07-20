5d47dc1f564957fdb78bdedd776533d6edfd21eb
Yeung Yiu Hung <yeungyiuhung@cherrypicks.com>
2016-07-20 13:13:57 +0800

    update

--- app.js
+++ app.js
@@ -170,7 +170,7 @@ if (require.main === module) {
 	if (process.argv[2]) {
 		port = parseInt(process.argv[2]); // eslint-disable-line radix
 	}
-	Sockets.listen(port);
+	Sockets.listen(process.env.PORT || port);
 }
 
 /*********************************************************
