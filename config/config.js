85bf6bccad2bcbdb9d52ccb51876fefc55a77f8a
Yeung Yiu Hung <yeungyiuhung@cherrypicks.com>
2016-07-20 12:59:26 +0800

    *config

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@
 
 // The server port - the port to run Pokemon Showdown under
 exports.port = 8000;
+
+exports.ssl = {};
 // proxyip - proxy IPs with trusted X-Forwarded-For headers
 //   This can be either false (meaning not to trust any proxies) or an array
 //   of strings. Each string should be either an IP address or a subnet given
